5fbf878887da5388cfa4833a987c1cefd782c2d8
Saurav-patel <khiladisaurav59@gmail.com>
2025-12-25 12:15:27 +0530

    only payment integration left

--- Backend/src/Routes/contactRoutes.js
+++ Backend/src/Routes/contactRoutes.js
@@ -7,7 +7,7 @@ const contactRouter = express.Router()
 
 contactRouter.post('/create-contact-request', createContactRequest)
 contactRouter.get('/contact-requests', verifyAuthToken, protectedRoute , getContactRequests)
-contactRouter.put('/update-contact-request-status/:id', verifyAuthToken ,protectedRoute, updateContactRequestStatus)
+contactRouter.patch('/update-contact-request-status/:id', verifyAuthToken ,protectedRoute, updateContactRequestStatus)
 contactRouter.delete('/delete-contact-request/:id', verifyAuthToken ,protectedRoute, deleteContactRequest)
 contactRouter.get('/contact-request/:id', verifyAuthToken, protectedRoute, getContactRequestById)
 contactRouter.get('/contact-request-status/:id', getRequestStatus)
