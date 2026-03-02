276d8e528082b70a716a7c57e79249604af161f7
Saurav-patel <khiladisaurav59@gmail.com>
2026-03-02 12:08:33 +0530

    fix bugs

--- Frontend/src/service/membershipService.js
+++ Frontend/src/service/membershipService.js
@@ -3,22 +3,22 @@ import { BaseService } from "./baseService.js";
 
 class MembershipService extends BaseService {
     async createMembership(planId) {
-        const res = await apiClient.post("/memberships/create", { planId });
+        const res = await apiClient.post("/membership/create", { planId });
         return this.validate(res, "Failed to create membership");
     }
 
     async getMembershipPlans() {
-        const res = await apiClient.get("/memberships/plans");
+        const res = await apiClient.get("/membership/plans");
         return this.parseData(res, "Failed to fetch membership plans");
     }
 
     async getMyMembership() {
-        const res = await apiClient.get("/memberships/my-membership");
+        const res = await apiClient.get("/membership/my-membership");
         return this.parseData(res, "Failed to fetch my membership");
     }
 
     async getAllMembers() {
-        const res = await apiClient.get("/memberships/all-members");
+        const res = await apiClient.get("/membership/all-members");
         return this.parseData(res, "Failed to fetch all members");
     }
 
