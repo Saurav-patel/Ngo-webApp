3179e0d6d9f03954c912df68e62447ccc076b753
Saurav-patel <khiladisaurav59@gmail.com>
2026-03-11 10:58:53 +0530

    fix bug

--- Backend/src/utils/aadharMasker.js
+++ Backend/src/utils/aadharMasker.js
@@ -1,9 +1,16 @@
 const maskAadhaar = (aadhaar) => {
-  const clean = aadhaar.replace(/\s+/g, "")
+
+  if (!aadhaar) {
+    throw new ApiError(400, "Aadhaar number is required")
+  }
+
+  const clean = String(aadhaar).replace(/\D/g, "")
+
   if (clean.length !== 12) {
     throw new ApiError(400, "Invalid Aadhaar number")
   }
 
   return `XXXX-XXXX-${clean.slice(-4)}`
 }
+
 export { maskAadhaar }
